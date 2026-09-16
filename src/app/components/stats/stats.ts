be9347239d0128531ed5bf55687b908ee902676c
Saif Kalkeri <kalkerisaif@gmail.com>
2026-09-17 00:35:25 +0530

    feat: add foundry stats and about sections

--- src/app/components/stats/stats.ts
+++ src/app/components/stats/stats.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
 
+interface FoundryStat {
+  value: string;
+  label: string;
+  description: string;
+}
+
 @Component({
-  imports: [],
   selector: 'app-stats',
-  styleUrl: './stats.scss',
+  standalone: true,
+  imports: [],
   templateUrl: './stats.html',
+  styleUrl: './stats.scss'
 })
-export class Stats {}
+export class Stats {
+  stats: FoundryStat[] = [
+    {
+      value: '25+',
+      label: 'Years of Excellence',
+      description: 'Engineering experience'
+    },
+    {
+      value: '30,000',
+      label: 'MT Annual Capacity',
+      description: 'Production capability'
+    },
+    {
+      value: '150+',
+      label: 'Components',
+      description: 'Developed & supplied'
+    },
+    {
+      value: '15+',
+      label: 'Countries',
+      description: 'Global market presence'
+    }
+  ];
+}
